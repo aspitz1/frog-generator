e87e05320cd014b921c123384d3efa5025c9b535
Anna Spitz <aspitzemail@gmail.com>
2022-04-19 20:52:48 -0500

    Update namefirst to be empty string

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const frogType = ['Grumpy', 'Sticky', 'Dizzy', 'Fast', 'Melting', 'Shook', 'Love Struck', 'Mopey', 'Nerdy', 'Bumpy', 'Long', 'Thick', 'Intellectual', 'Grainy', 'Hypno', 'Slow', 'Granny', 'Crafty', 'Hamburger', 'Pie', 'Sparkle', 'Stump', 'Bumble', 'Chef', 'Sumo', 'Traveler'];
-let nameFirst = 'Teapot';
+let nameFirst = '';
 console.log(`${frogType[nameFirst.charCodeAt(0) - 65]} Frog!`);
