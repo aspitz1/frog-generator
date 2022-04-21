957b818f785e659a7768a805236ba04f57fdce61
Anna Spitz <aspitzemail@gmail.com>
2022-04-20 19:32:13 -0500

    Add script functionality

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// array of frog names
 const frogType = ['Grumpy', 'Sticky', 'Dizzy', 'Fast', 'Melting', 'Shook', 'Love Struck', 'Mopey', 'Nerdy', 'Bumpy', 'Long', 'Thick', 'Intellectual', 'Grainy', 'Hypno', 'Slow', 'Granny', 'Crafty', 'Hamburger', 'Pie', 'Sparkle', 'Stump', 'Bumble', 'Chef', 'Sumo', 'Traveler'];
-let nameFirst = '';
-console.log(`${frogType[nameFirst.charCodeAt(0) - 65]} Frog!`);
+
+const form = document.querySelector('#form')
+const output = document.querySelector('#frogName')
+// gathers input and matches it to array index value based on the first letter of the name
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  const nameFirst = event.target[0].value[0].toUpperCase();
+// frog name output
+  output.textContent = `${frogType[nameFirst.charCodeAt(0) - 65]} Frog!`;
+})
+form.addEventListener('reset', function(event){
+  output.textContent = '';
+})
