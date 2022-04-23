2c3c24fd0d43b8bea9bcd2dcf626568be846a600
Anna Spitz <aspitzemail@gmail.com>
2022-04-23 15:05:10 -0500

    Add image script

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // array of frog names
 const frogType = ['Grumpy', 'Sticky', 'Dizzy', 'Fast', 'Melting', 'Shook', 'Love Struck', 'Mopey', 'Nerdy', 'Bumpy', 'Long', 'Thick', 'Intellectual', 'Grainy', 'Hypno', 'Slow', 'Granny', 'Crafty', 'Hamburger', 'Pie', 'Sparkle', 'Stump', 'Bumble', 'Chef', 'Sumo', 'Traveler'];
 
-const form = document.querySelector('#form')
-const output = document.querySelector('#frogName')
-const button = document.querySelector('.rese')
+const form = document.querySelector('#form');
+const output = document.querySelector('#frogName');
+const img = document.querySelector('#frogImg');
 // gathers input and matches it to array index value based on the first letter of the name
 form.addEventListener('submit', function(event){
   event.preventDefault();
   const nameFirst = event.target[0].value[0].toUpperCase();
 // frog name output
-  output.textContent = `${frogType[nameFirst.charCodeAt(0) - 65]} Frog!`;
+  const currentFrog = frogType[nameFirst.charCodeAt(0) - 65]
+  output.textContent = `${currentFrog} Frog!`;
+  img.src = `Frogs/${currentFrog.split(' ').join('-')}.jpg`;
 })
 form.addEventListener('reset', function(event){
   output.textContent = '';
+  img.src = 'Frogs/Base.jpg';
 })
