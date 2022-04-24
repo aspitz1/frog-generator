f03c35ffb7537c03faafe9e31ab6dff314ff2ae7
Anna Spitz <aspitzemail@gmail.com>
2022-04-23 19:09:35 -0500

    Fix typo in jpg file names

---
+++ README.md
@@ -0,0 +1,4 @@
+# Frog Generator
+This cute app will match you with a frog based on the first letter of your first name. It will display the type of frog you've been matched with, and a cute little image (made by me, Anna Spitz!) :frog:
+## How it's Made
+The output is based on an array of 26 items. There is a `eventListener` that uses a function to take the input value at 0 and turn it into a capital letter in order to use its charset - 65 to call a value in the array. 
--- script.js
+++ script.js
@@ -7,13 +7,16 @@ const img = document.querySelector('#frogImg');
 // gathers input and matches it to array index value based on the first letter of the name
 form.addEventListener('submit', function(event){
   event.preventDefault();
+// event.target is the form event.target at 0 is the imput 
   const nameFirst = event.target[0].value[0].toUpperCase();
 // frog name output
   const currentFrog = frogType[nameFirst.charCodeAt(0) - 65]
   output.textContent = `${currentFrog} Frog!`;
+// changes image and alt text
   img.src = `Frogs/${currentFrog.split(' ').join('-')}.jpg`;
   img.alt = `A ${currentFrog} Frog!`
 })
+// resets form
 form.addEventListener('reset', function(event){
   output.textContent = '';
   img.src = 'Frogs/Base.jpg';
