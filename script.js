b9828ab115f069835ef64605b985cb05ad451298
Anna Spitz <aspitzemail@gmail.com>
2022-04-24 21:35:22 -0500

    Add if statement in script checking for undefined

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ const img = document.querySelector('#frogImg');
 // gathers input and matches it to array index value based on the first letter of the name
 form.addEventListener('submit', function(event){
   event.preventDefault();
-// event.target is the form event.target at 0 is the imput 
+// event.target is the form event.target at 0 is the imput
   const nameFirst = event.target[0].value[0].toUpperCase();
 // frog name output
   const currentFrog = frogType[nameFirst.charCodeAt(0) - 65]
+  if (currentFrog === undefined) {
+  output.textContent = 'Only Letters Please';
+} else {
   output.textContent = `${currentFrog} Frog!`;
+}
 // changes image and alt text
   img.src = `Frogs/${currentFrog.split(' ').join('-')}.jpg`;
   img.alt = `A ${currentFrog} Frog!`
